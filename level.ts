3fefbdba3e5f0927532b33c0264c5f7b0f1e7899
Carl Walsh <darthwalsh@gmail.com>
2017-03-13 08:58:38 -0700

    working GetSurface

--- level.ts
+++ level.ts
@@ -3,10 +3,12 @@
 module Level {
   export function GetSurface(dim: number, level: number): number[][] {
     let set : number[] = [];
-    for (let i = level; i >= -level; i -= 2) {
+    for (let i = -level; i <= level; i += 2) {
       set.push(i);
     }
-    return [];
+    let perms = MultiPermutations(set, dim);
+    return perms.filter(arr => 
+      arr.indexOf(level) >= 0 || arr.indexOf(-level) >= 0);
   }
 
   function MultiPermutations<T>(arr: T[], count: number): T[][] {
@@ -25,12 +27,39 @@ module Level {
     return ans;
   }
 
+/*
   export function Test() {
     console.log(JSON.stringify(MultiPermutations([0,1], 3)));
 
     const expected = [
       {d:1, l:1, e:[[-1], [1]]},
       {d:1, l:2, e:[[-2], [2]]},
+      {d:2, l:1, e:[
+        [-1, -1],
+        [-1,  1],
+        [ 1, -1],
+        [ 1,  1],
+        ]},
+      {d:2, l:2, e:[
+        [-2, -2],
+        [-2,  0],
+        [-2,  2],
+        [ 0, -2],
+        [ 0,  2],
+        [ 2, -2],
+        [ 2,  0],
+        [ 2,  2],
+        ]},
+      {d:3, l:1, e:[
+        [-1, -1, -1],
+        [-1, -1,  1],        
+        [-1,  1, -1],
+        [-1,  1,  1],
+        [ 1, -1, -1],
+        [ 1, -1,  1],
+        [ 1,  1, -1],
+        [ 1,  1,  1],
+        ]},
     ]
 
     for (let i of expected) {
@@ -42,6 +71,8 @@ module Level {
   function AreEqual(expected: any, actual: any) {
     expected = JSON.stringify(expected);
     actual = JSON.stringify(actual);
-    console.assert(expected === actual, `${expected} !== ${actual}`)
+    console.assert(expected === actual, `${expected} !== ${actual}`);
+    console.log(`${expected} passed`);
   }
+*/
 }
